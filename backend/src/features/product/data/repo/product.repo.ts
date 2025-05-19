f20493aea4765d69719b61ed8da664ac8dde61a7
Youngjae <2015fromjeki@gmail.com>
2025-05-19 16:00:35 +1000

    Feat: Implement data layer

---
+++ backend/src/features/product/data/repo/product.repo.ts
@@ -0,0 +1,7 @@
+import Post from "../entities/product.entity";
+export interface PostRepo {
+  fetchAllProducts(): Promise<Post[]>;
+  fetchSingleProduct(id: string): Promise<Post>;
+  updateSingleProduct(id: string): Promise<Post>;
+  deleteSingleProduct(id: string): Promise<void>;
+}
